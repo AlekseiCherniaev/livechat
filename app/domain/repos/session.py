17f30d96c986351a61b54a7e464574b6bf2b6ef3
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-10-10 18:06:14 +0300

    feat(session): add methods to list and delete sessions by user ID

--- app/domain/repos/session.py
+++ app/domain/repos/session.py
@@ -11,10 +11,16 @@ class SessionRepository(Protocol):
     async def get(self, session_id: UUID) -> UserSession | None:
         pass
 
+    async def list_by_user_id(self, user_id: UUID) -> list[UserSession]:
+        pass
+
     async def update(self, session: UserSession) -> None:
         pass
 
     async def delete_by_id(self, session_id: UUID) -> None:
+        pass
+
+    async def delete_by_user_id(self, user_id: UUID) -> None:
         pass
 
     async def is_online(self, user_id: UUID) -> bool:
