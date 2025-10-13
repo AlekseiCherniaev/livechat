faefe101f042c208c4e00c8befbbbcc0e1423e17
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-10-13 21:38:19 +0300

    feat(dependency-injection): add Cassandra message repository

--- app/api/di.py
+++ app/api/di.py
@@ -1,15 +1,21 @@
 from fastapi import Request, Depends
 
 from app.adapters.db.repos.chat_room import MongoChatRoomRepository
+from app.adapters.db.repos.message_repo import CassandraMessageRepository
 from app.adapters.db.repos.notification import MongoNotificationRepository
 from app.adapters.db.repos.session import RedisSessionRepository
 from app.adapters.db.repos.user import MongoUserRepository
 from app.domain.ports.password_hasher import PasswordHasherPort
 from app.domain.repos.chat_room import ChatRoomRepository
+from app.domain.repos.message import MessageRepository
 from app.domain.repos.notification import NotificationRepository
 from app.domain.repos.session import SessionRepository
 from app.domain.repos.user import UserRepository
 from app.domain.services.user_service import UserService
+
+
+def get_password_hasher(request: Request) -> PasswordHasherPort:
+    return request.app.state.password_hasher  # type: ignore
 
 
 def get_user_repo(request: Request) -> UserRepository:
@@ -28,8 +34,8 @@ def get_chat_repo(request: Request) -> ChatRoomRepository:
     return MongoChatRoomRepository(db=request.app.state.mongo_db)
 
 
-def get_password_hasher(request: Request) -> PasswordHasherPort:
-    return request.app.state.password_hasher  # type: ignore
+def get_message_repo() -> MessageRepository:
+    return CassandraMessageRepository()
 
 
 def get_user_service(
