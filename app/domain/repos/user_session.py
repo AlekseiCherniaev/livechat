3ee9ee2e4013daf2b34d1a95d0d93cb822f09547
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-10-15 00:42:49 +0300

    refactor(user_session): simplify method definitions in UserSessionRepository

--- app/domain/repos/user_session.py
+++ app/domain/repos/user_session.py
@@ -5,20 +5,14 @@ from app.domain.entities.user_session import UserSession
 
 
 class UserSessionRepository(Protocol):
-    async def save(self, session: UserSession) -> None:
-        pass
+    async def save(self, session: UserSession) -> None: ...
 
-    async def get(self, session_id: UUID) -> UserSession | None:
-        pass
+    async def get(self, session_id: UUID) -> UserSession | None: ...
 
-    async def list_by_user_id(self, user_id: UUID) -> list[UserSession]:
-        pass
+    async def list_by_user_id(self, user_id: UUID) -> list[UserSession]: ...
 
-    async def update(self, session: UserSession) -> None:
-        pass
+    async def update(self, session: UserSession) -> None: ...
 
-    async def delete_by_id(self, session_id: UUID) -> None:
-        pass
+    async def delete_by_id(self, session_id: UUID) -> None: ...
 
-    async def delete_by_user_id(self, user_id: UUID) -> None:
-        pass
+    async def delete_by_user_id(self, user_id: UUID) -> None: ...
