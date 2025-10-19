e2c6d9f2bfbb5645b9a07f5fc2f0c8544b08b63e
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-10-19 12:44:16 +0300

    refactor(status): conditionally expose Swagger UI in non-production

--- app/api/routes/status.py
+++ app/api/routes/status.py
@@ -2,18 +2,24 @@ from fastapi import APIRouter, Response, status
 from fastapi.openapi.docs import get_swagger_ui_html
 from starlette.responses import HTMLResponse
 
+from app.core.constants import Environment
+from app.core.settings import get_settings
+
 router = APIRouter(tags=["status"])
 
+is_production = get_settings().environment == Environment.PROD
 
-@router.get("/internal/docs", include_in_schema=False)
-async def custom_swagger_ui_html() -> HTMLResponse:
-    return get_swagger_ui_html(
-        openapi_url="/api/internal/openapi.json",
-        title="Livechat API",
-        swagger_css_url="static/swagger-ui.css",
-        swagger_js_url="static/swagger-ui-bundle.js",
-        swagger_favicon_url="static/fastapi.png",
-    )
+if not is_production:
+
+    @router.get("/internal/docs", include_in_schema=False)
+    async def custom_swagger_ui_html() -> HTMLResponse:
+        return get_swagger_ui_html(
+            openapi_url="/api/internal/openapi.json",
+            title="Livechat API",
+            swagger_css_url="static/swagger-ui.css",
+            swagger_js_url="static/swagger-ui-bundle.js",
+            swagger_favicon_url="static/fastapi.png",
+        )
 
 
 @router.get("/health")
