0a5b67123fbbe6a0fdd4d5f931ca69f585cc7467
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-10-29 12:56:02 +0300

    refactor(middlewares): remove TrustedHostMiddleware configuration

--- app/api/middlewares.py
+++ app/api/middlewares.py
@@ -2,7 +2,6 @@ import structlog
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.gzip import GZipMiddleware
-from starlette.middleware.trustedhost import TrustedHostMiddleware
 
 from app.core.settings import get_settings
 
@@ -10,10 +9,6 @@ logger = structlog.getLogger(__name__)
 
 
 def add_middlewares(app: FastAPI) -> None:
-    app.add_middleware(
-        TrustedHostMiddleware, allowed_hosts=[f"*.{get_settings().domain}"]
-    )
-
     app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=6)
 
     app.add_middleware(
